e82be11b820af217c19f319bedc19ffa6bcc7937
Manuel Pérez <alexsamboy@gmail.com>
2021-03-22 10:43:27 -0400

    Cambios economía

--- js/carrera.js
+++ js/carrera.js
@@ -1,39 +1,43 @@
 // JavaScript Document
 
-var formCarrera = document.getElementById("text-00000010");
+if (document.readyState === 'complete'){
+    var formCarrera = document.getElementById("text-00000008");
 
-function cambiarCarrera(a){
+function cambiarCarrera(a) {
 
-    if(a !=""){
-        switch (a) {
-  case 'Licenciatura en Marketing':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-  case 'Licenciatura en Gestión Financiera y Auditoría':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-  case 'Licenciatura Dirección Empresarial':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-    case 'Licenciatura en Economía':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-    case 'Licenciatura en Hospitalidad y Turismo':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-    case 'Licenciatura en Administración Hotelera':
-    formCarrera.value = a;
-    console.log(formCarrera.value);
-    break;
-  default:
-    console.log(`Sorry, we are out of ${formCarrera}.`);
-}
+  if (a != "") {
+    switch (a) {
+      case 'Licenciatura en Marketing':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+        break;
+      case 'Licenciatura en Gestión Financiera y Auditoría':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+        break;
+      case 'Licenciatura Dirección Empresarial':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+        break;
+      case 'Licenciatura en Economía':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+        break;
+      case 'Licenciatura en Hospitalidad y Turismo':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+        break;
+      case 'Licenciatura en Administración Hotelera':
+        formCarrera.value = a;
+        console.log(formCarrera.value);
+            console.log(a);
+        break;
+      default:
+        console.log(`Sorry, we are out of ${formCarrera}.`);
     }
+  }
 }
 
-cambiarCarrera(programa);
+    console.log(programa);
+
+}
